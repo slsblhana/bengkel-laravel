dfa751997741907fbcf330d9fc1dea6466050b43
Salsabila Hana Suhaimah <suhaimahsalsa98@gmail.com>
2019-03-30 15:57:11 +0800

    first commit

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,12 @@
+<ul>
+    @foreach ($posts as $post)
+    <li>
+        <a href="{{ route('post.edit',$post) }}">
+        {{ $post->title }}
+        </a>
+        <img width="200" src="{{ asset('storage/'.$post->image) }}" alt="">
+        </li>
+    @endforeach
+</ul>
+
+<a href="{{route('post.create')}}">Add post</a>
